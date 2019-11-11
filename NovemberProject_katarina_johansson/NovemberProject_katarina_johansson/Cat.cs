6b70b224968a147ba0d474656d2a7790fd3d0e35
Vixelus <55139186+Vixelus@users.noreply.github.com>
2019-11-11 11:05:42 +0100

    I did the menu today
    i did the menu today.

---
+++ NovemberProject_katarina_johansson/NovemberProject_katarina_johansson/Cat.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovemberProject_katarina_johansson
+{
+    class Cat
+    {
+
+        public string name;
+           private  string cuteness;
+           private  string Power;
+        public int howmany;
+
+        static string[] option = { " Start", " Quit" };
+        static int index = 0;
+        static string[] fight = { " Buy", "Pet", " Quit" + "" };
+
+
+
+        public static int Selection()
+        {
+            while (true)
+            {
+                for (int i = 0; i < option.Length; i++)
+                {
+                    string text = "";
+                    if (index == i)
+                        text += ">";
+                    text += option[i];
+                    Console.WriteLine(text);
+                }
+                ConsoleKeyInfo input = Console.ReadKey(); //get a key input from the player
+                if (input.Key == ConsoleKey.UpArrow) //go up
+                    index--;
+                if (input.Key == ConsoleKey.DownArrow) //go down
+                    index++;
+                if (input.Key == ConsoleKey.Enter)
+                {
+
+                    return index;
+                }
+
+                Console.Clear();
+                if (index <= 0)
+                {
+                    index = 0;
+                    Console.Clear();
+                }
+
+                if (index >= 1)
+
+                {
+                    index = 1;
+
+
+                }
+            }
+        }
+        public void Names()
+        {
+           
+            string length = Console.ReadLine();
+           List<string> Catnames = new List<string>();
+
+           int length = Catnames.Count;
+           
+        }
+      
+
+
+    }
+}
--- NovemberProject_katarina_johansson/NovemberProject_katarina_johansson/Program.cs
+++ NovemberProject_katarina_johansson/NovemberProject_katarina_johansson/Program.cs
@@ -10,6 +10,26 @@ namespace NovemberProject_katarina_johansson
     {
         static void Main(string[] args)
         {
+            Cat c1 = new Cat();
+            Console.WriteLine("Welcome to the cat collector! :3");
+            Console.WriteLine("You have 10G");
+            Console.ReadKey();
+            Console.WriteLine("Without food cats won't visit you");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Would you like to buy food?");
+            Console.WriteLine("A) Yes B) No");
+           string answer =  Console.ReadLine();
+            string A = "A";
+            if ( answer = A)
+            {
+               Console.WriteLine("How much food do you want to buy?");
+
+            }
+                   
+
+            
+
         }
     }
 }
